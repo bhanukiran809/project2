22285dab8cabf7c72c1515685b8bb3ec478de210
Bhanu Kiran <bhanukiran809@gmail.com>
2022-10-29 13:58:23 +0530

    added second h1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
      <h1>i am kiran doing project2</h1>
+     <h1>i am kiran doing project3</h1>
+     <h1>i am kiran doing project4</h1>
+     <h1>i am kiran doing project5</h1>
     </div>
   );
 }
